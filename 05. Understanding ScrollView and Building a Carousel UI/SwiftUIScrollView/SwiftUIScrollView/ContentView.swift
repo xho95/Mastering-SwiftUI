a70d01357cd76b8a1c7e39c6aba943c00254c053
Kim, Min Ho <xho1995@gmail.com>
2021-04-21 21:52:51 +0900

    makes the header on the ScrollView in the chapter 05

--- 05. Understanding ScrollView and Building a Carousel UI/SwiftUIScrollView/SwiftUIScrollView/ContentView.swift
+++ 05. Understanding ScrollView and Building a Carousel UI/SwiftUIScrollView/SwiftUIScrollView/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    func refineDate() -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter.string(from: Date())
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("\(refineDate())".uppercased())
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    Text("Your Reading")
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                }
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+
+            VStack {
+                CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
+                CardView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
+                CardView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
+                CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+            }
+        }
     }
 }
 
