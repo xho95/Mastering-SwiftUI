6f1d464ab0982de9043f89f4bd657e1b7190f1d4
Kim, Min Ho <xho1995@gmail.com>
2021-04-21 21:24:51 +0900

    makes the CardView with solution of the Book

---
+++ 05. Understanding ScrollView and Building a Carousel UI/SwiftUIScrollView/SwiftUIScrollView/CardView.swift
@@ -0,0 +1,54 @@
+//
+//  CardView.swift
+//  SwiftUIScrollView
+//
+//  Created by Kim, Min Ho on 2021/04/21.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var image: String
+    var category: String
+    var heading: String
+    var author: String
+    
+    var body: some View {
+        VStack {
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(category)
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    Text(heading)
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                    Text("Written by \(author)".uppercased())
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+
+                Spacer()
+            }
+            .padding()
+        }
+        .cornerRadius(10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255), lineWidth: 1)
+        )
+        .padding([.top, .horizontal])
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Kim, Min Ho")
+    }
+}
---
+++ 05. Understanding ScrollView and Building a Carousel UI/SwiftUIScrollView/SwiftUIScrollView/SwiftUIScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScrollViewApp.swift
+//  SwiftUIScrollView
+//
+//  Created by Kim, Min Ho on 2021/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
