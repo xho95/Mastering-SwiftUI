3e7ff543ca271ecda770605361f7c45107f5b0e0
Kim, Min Ho <xho1995@gmail.com>
2021-04-22 22:30:48 +0900

    finishes the chapter 09 without solving two exercises

---
+++ 09. Basic Animations and Transitions/SwiftUITransition/SwiftUITransition/ContentView.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  SwiftUITransition
+//
+//  Created by Kim, Min Ho on 2021/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var show = false
+    
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 300, height: 300)
+                .foregroundColor(.green)
+                .overlay(
+                    Text("Show details")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .bold()
+                        .foregroundColor(.white)
+                )
+            
+            if show {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 300, height: 300)
+                    .foregroundColor(.purple)
+                    .overlay(
+                        Text("Well, here is the details")
+                            .font(.system(.largeTitle, design: .rounded))
+                            .bold()
+                            .foregroundColor(.white)
+                    )
+                    //.transition(.scale(scale: 0, anchor: .bottom))
+                    //.transition(.offset(x: -600, y: 0))
+                    //.transition(AnyTransition.offset(x: -600, y: 0).combined(with: .scale))
+                    //.transition(AnyTransition.offset(x: -600, y: 0).combined(with: .scale).combined(with: .opacity))
+                    //.transition(.offsetScaleOpacity)
+                    .transition(.scaleAndOffset)
+            }
+        }
+        .onTapGesture {
+            withAnimation(Animation.spring()) {
+                self.show.toggle()
+            }
+        }
+    }
+}
+
+extension AnyTransition {
+    static var offsetScaleOpacity: AnyTransition {
+        AnyTransition
+            .offset(x: -600, y: 0)
+            .combined(with: .scale)
+            .combined(with: .opacity)
+    }
+    
+    static var scaleAndOffset: AnyTransition {
+        AnyTransition
+            .asymmetric(
+                insertion: .scale(scale: 0, anchor: .bottom),
+                removal: .offset(x: -600, y: 0)
+            )
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 09. Basic Animations and Transitions/SwiftUITransition/SwiftUITransition/SwiftUITransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITransitionApp.swift
+//  SwiftUITransition
+//
+//  Created by Kim, Min Ho on 2021/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
