3553cae52ae8a06350cdb09cf4ffe63ca8699505
Kim, Min Ho <xho1995@gmail.com>
2021-04-22 19:23:49 +0900

    adds some additional sample code by using the Built-in Shapes

--- 08. Implementing Path and Shape for Line Drawing and Pie Charts/SwiftUIShape/SwiftUIShape/DomeButton.swift
+++ 08. Implementing Path and Shape for Line Drawing and Pie Charts/SwiftUIShape/SwiftUIShape/DomeButton.swift
@@ -9,15 +9,26 @@ import SwiftUI
 
 struct DomeButton: View {
     var body: some View {
-        Button {
-            //
-        } label: {
-            Text("Test")
-                .font(.system(.title, design: .rounded))
-                .bold()
-                .foregroundColor(.white)
-                .frame(width: 250, height: 50)
-                .background(Dome().fill(Color.red))
+        VStack {
+            Button {
+                //
+            } label: {
+                Text("Test")
+                    .font(.system(.title, design: .rounded))
+                    .bold()
+                    .foregroundColor(.white)
+                    .frame(width: 250, height: 50)
+                    .background(Dome().fill(Color.red))
+            }
+            
+            Circle()
+                .foregroundColor(.green)
+                .frame(width: 200, height: 200)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                )
         }
     }
 }
