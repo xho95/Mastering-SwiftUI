2f3a894cde782bc0607776389847ba289d2df33e
Kim, Min Ho <xho1995@gmail.com>
2021-04-22 21:53:00 +0900

    migrates the codes from SwiftUIRotation to SwiftUIIndicator

---
+++ 09. Basic Animations and Transitions/SwiftUIIndicator/SwiftUIIndicator/SwiftUIIndicatorApp.swift
@@ -0,0 +1,26 @@
+//
+//  SwiftUIIndicatorApp.swift
+//  SwiftUIIndicator
+//
+//  Created by Kim, Min Ho on 2021/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VStack {
+                CircularIndicator()
+                    .padding(.vertical, 25)
+                LinearIndicator()
+                    .padding(.vertical, 25)
+                ProgressIndicator()
+                    .padding(.vertical, 25)
+                DotIndicator()
+                    .padding(.vertical, 25)
+            }
+        }
+    }
+}
