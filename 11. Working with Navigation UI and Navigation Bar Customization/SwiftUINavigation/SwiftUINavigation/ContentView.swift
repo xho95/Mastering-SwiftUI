20390c52aab747112c81415f9966616ba8c2906f
Kim, Min Ho <xho1995@gmail.com>
2021-04-23 22:26:44 +0900

    refactors the exercise code before the performing the task

--- 11. Working with Navigation UI and Navigation Bar Customization/SwiftUINavigation/SwiftUINavigation/ContentView.swift
+++ 11. Working with Navigation UI and Navigation Bar Customization/SwiftUINavigation/SwiftUINavigation/ContentView.swift
@@ -8,15 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     var body: some View {
-        
         List(articles) { article in
-                
             ArticleRow(article: article)
-                
         }
- 
     }
 }
 
@@ -27,39 +22,4 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 
-struct ArticleRow: View {
-    var article: Article
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-            Image(article.image)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(5)
-            
-            Text(article.title)
-                .font(.system(.title, design: .rounded))
-                .fontWeight(.black)
-                .lineLimit(3)
-                .padding(.bottom, 0)
-            
-            Text("By \(article.author)".uppercased())
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                .padding(.bottom, 0)
-            
-            HStack(spacing: 3) {
-                ForEach(1...(article.rating), id: \.self) { _ in
-                    Image(systemName: "star.fill")
-                        .font(.caption)
-                        .foregroundColor(.yellow)
-                }
-            }
-            
-            Text(article.excerpt)
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-            
-        }
-    }
-}
+
