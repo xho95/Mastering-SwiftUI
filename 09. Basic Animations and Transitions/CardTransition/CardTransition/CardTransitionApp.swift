71edbeb60c2c3cc857f45b2f962ec3f186f8d08a
Kim, Min Ho <xho1995@gmail.com>
2021-04-23 15:43:45 +0900

    initializes the exercise 02 in the chapter 09

---
+++ 09. Basic Animations and Transitions/CardTransition/CardTransition/CardTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardTransitionApp.swift
+//  CardTransition
+//
+//  Created by Kim, Min Ho on 2021/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CardTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
