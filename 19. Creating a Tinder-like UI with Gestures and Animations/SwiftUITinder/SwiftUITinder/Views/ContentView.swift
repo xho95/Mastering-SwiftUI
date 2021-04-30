f37223c7c609487a8fe0fabdd2bc95dc9e107f54
Kim, Min Ho <xho1995@gmail.com>
2021-04-30 15:15:43 +0900

    initializes the project of the chapter 19

---
+++ 19. Creating a Tinder-like UI with Gestures and Animations/SwiftUITinder/SwiftUITinder/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITinderTrip
+//
+//  Created by Simon Ng on 24/8/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
