370dbc99f47519cb18ecb73b0cf49016fb44981e
Kim, Min Ho <xho1995@gmail.com>
2021-05-01 16:30:11 +0900

    implements the Drag Gesture on the project in the chapter 19

---
+++ 19. Creating a Tinder-like UI with Gestures and Animations/SwiftUITinder/SwiftUITinder/Models/DragState.swift
@@ -0,0 +1,39 @@
+//
+//  DragState.swift
+//  SwiftUITinder
+//
+//  Created by Kim, Min Ho on 2021/05/01.
+//
+
+import SwiftUI
+
+enum DragState {
+    case inactive, pressing, dragging(translation: CGSize)
+    
+    var translation: CGSize {
+        switch self {
+        case .inactive, .pressing:
+            return .zero
+        case .dragging(let translation):
+            return translation
+        }
+    }
+    
+    var isDragging: Bool {
+        switch self {
+        case .dragging:
+            return true
+        case .pressing, .inactive:
+            return false
+        }
+    }
+    
+    var isPressing: Bool {
+        switch self {
+        case .pressing, .dragging:
+            return true
+        case .inactive:
+            return false
+        }
+    }
+}
--- 19. Creating a Tinder-like UI with Gestures and Animations/SwiftUITinder/SwiftUITinder/Views/ContentView.swift
+++ 19. Creating a Tinder-like UI with Gestures and Animations/SwiftUITinder/SwiftUITinder/Views/ContentView.swift
@@ -8,16 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @GestureState private var dragState = DragState.inactive
+    
+    // TODO: Move this code into the ViewModel corresponding with the Generic Model
+    var cardViews: [CardView] = {
+        var views = [CardView]()
+        
+        // a resource efficient way to implement the card deck
+        for index in 0..<2 {
+            views.append(CardView(image: trips[index].image, title: trips[index].destination))
+        }
+        
+        return views
+    }()
+    
     var body: some View {
         VStack {
             TopBarMenu()
             
-            CardView(image: trips[0].image, title: trips[0].destination)
+            ZStack {
+                ForEach(cardViews) { cardView in
+                    cardView
+                        .zIndex(self.isTopCard(cardView: cardView) ? 1 : 0)
+                        .offset(
+                            x: self.isTopCard(cardView: cardView) ? self.dragState.translation.width : 0,
+                            y: self.isTopCard(cardView: cardView) ? self.dragState.translation.height : 0
+                        )
+                        .scaleEffect(
+                            self.isTopCard(cardView: cardView) && self.dragState.isDragging ? 0.95 : 1.0
+                        )
+                        .rotationEffect(
+                            Angle(degrees: self.isTopCard(cardView: cardView) ?
+                                    Double( self.dragState.translation.width / 10) : 0)
+                        )
+                        .animation(.interpolatingSpring(stiffness: 180, damping: 100))
+                        .gesture(
+                            LongPressGesture(minimumDuration: 0.01)
+                                .sequenced(before: DragGesture())
+                                .updating(self.$dragState) { value, state, transaction in
+                                    switch value {
+                                    case .first(true):
+                                        state = .pressing
+                                    case .second(true, let drag):
+                                        state = .dragging(translation: drag?.translation ?? .zero)
+                                    default:
+                                        break
+                                    }
+                                }
+                        )
+                }
+            }
             
             Spacer(minLength: 20)
             
             BottomBarMenu()
+                .opacity(dragState.isDragging ? 0.0 : 1.0)
+                .animation(.default)
         }
+    }
+    
+    private func isTopCard(cardView: CardView) -> Bool {
+        guard let index = cardViews.firstIndex(where: { $0.id == cardView.id }) else {
+            return false
+        }
+        
+        return index == 0
     }
 }
 
