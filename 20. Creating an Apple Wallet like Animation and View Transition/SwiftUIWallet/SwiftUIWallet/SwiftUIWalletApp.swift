e0f97c7d7910d81c9b46f5c0a2736472a1bbe2e7
Kim, Min Ho <xho1995@gmail.com>
2021-05-02 14:06:48 +0900

    downloads and initializes the project of the chapter 20

---
+++ 20. Creating an Apple Wallet like Animation and View Transition/SwiftUIWallet/SwiftUIWallet/SwiftUIWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWalletApp.swift
+//  SwiftUIWallet
+//
+//  Created by Simon Ng on 27/8/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 20. Creating an Apple Wallet like Animation and View Transition/SwiftUIWallet/SwiftUIWallet/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIWallet
+//
+//  Created by Simon Ng on 27/8/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
