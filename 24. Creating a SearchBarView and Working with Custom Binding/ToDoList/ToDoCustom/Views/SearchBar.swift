0d4547a596a899edc2fdef184f009a870f5f855d
Kim, Min Ho <xho1995@gmail.com>
2021-05-07 22:01:53 +0900

    replaces the SearchBar from UIView to SwiftUI

--- 24. Creating a SearchBarView and Working with Custom Binding/ToDoList/ToDoCustom/Views/SearchBar.swift
+++ 24. Creating a SearchBarView and Working with Custom Binding/ToDoList/ToDoCustom/Views/SearchBar.swift
@@ -7,8 +7,65 @@
 
 import SwiftUI
 
-struct SearchBar: UIViewRepresentable {
+struct SearchBar: View {
+    @Binding var text: String
 
+    @State private var isEditing = false
+    
+    var body: some View {
+        HStack {
+            TextField("Search ...", text: $text)
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+                        
+                        if self.isEditing {
+                            Button {
+                                self.text = ""
+                            } label: {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(.gray)
+                                    .padding(.trailing, 8)
+                            }
+                        }
+                    }
+                )
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    self.isEditing = true
+                }
+            
+            if self.isEditing {
+                Button {
+                    self.isEditing = false
+                    self.text = ""
+                } label: {
+                    Text("Cancel")
+                }
+                .padding(.trailing, 10)
+                .transition(.move(edge: .trailing))
+                .animation(.default)
+            }
+        }
+    }
+}
+
+struct SearchBar_Preview: PreviewProvider {
+    static var previews: some View {
+        SearchBar(text: .constant(""))
+    }
+}
+
+
+/*
+struct SearchBarOld: UIViewRepresentable {
     @Binding var text: String
     
     func makeUIView(context: Context) -> UISearchBar {
@@ -61,3 +118,4 @@ struct SearchBar: UIViewRepresentable {
         }
     }
 }
+*/
