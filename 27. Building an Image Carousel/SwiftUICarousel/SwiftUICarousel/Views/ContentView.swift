3fb5344a60f027adfcf85b146fe1c6a525b058de
Kim, Min Ho <xho1995@gmail.com>
2021-05-10 00:03:09 +0900

    adds the TripDetail View code in the project

--- 27. Building an Image Carousel/SwiftUICarousel/SwiftUICarousel/Views/ContentView.swift
+++ 27. Building an Image Carousel/SwiftUICarousel/SwiftUICarousel/Views/ContentView.swift
@@ -14,37 +14,52 @@ struct ContentView: View {
     @GestureState private var dragOffset: CGFloat = 0
     
     var body: some View {
-        GeometryReader { outer in
-            HStack(spacing: 0) {
-                ForEach(sampleTrips.indices) { index in
-                    GeometryReader { inner in
-                        TripCardView(destination: sampleTrips[index].destination,
-                                     imageName: sampleTrips[index].image,
-                                 isShowDetails: $isCardTapped)
-                    }
-                    .padding(.horizontal, self.isCardTapped ? CGFloat(0) : 20)
-                    .opacity(currentTripIndex == index ? 1.0 : 0.7)
-                    .frame(width: outer.size.width, height: currentTripIndex == index ? (isCardTapped ? outer.size.height : 450) : 400)
-                }
+        ZStack {
+            VStack(alignment: .leading) {
+                Text("Discover")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.black)
+                Text("Explore your next destination")
+                    .font(.system(.headline, design: .rounded))
             }
-            .frame(width: outer.size.width, height: outer.size.height, alignment: .leading)
-            .offset(x: -CGFloat(currentTripIndex) * outer.size.width)
-            .offset(x: dragOffset)
-            .gesture(
-                !isCardTapped ? DragGesture()
-                    .updating($dragOffset) { value, state, transaction in
-                        state = value.translation.width
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+            .padding(.top, 25)
+            .padding(.leading, 20)
+            .opacity(isCardTapped ? 0.1 : 1.0)
+            .offset(y: isCardTapped ? -100 : 0)
+            
+            GeometryReader { outer in
+                HStack(spacing: 0) {
+                    ForEach(sampleTrips.indices) { index in
+                        GeometryReader { inner in
+                            TripCardView(destination: sampleTrips[index].destination,
+                                         imageName: sampleTrips[index].image,
+                                     isShowDetails: $isCardTapped)
+                        }
+                        .padding(.horizontal, self.isCardTapped ? CGFloat(0) : 20)
+                        .opacity(currentTripIndex == index ? 1.0 : 0.7)
+                        .frame(width: outer.size.width, height: currentTripIndex == index ? (isCardTapped ? outer.size.height : 450) : 400)
                     }
-                    .onEnded { value in
-                        let threshold = outer.size.width * 0.65
-                        var newIndex = Int(-value.translation.width / threshold) + currentTripIndex
-                        newIndex = min(max(newIndex, 0), sampleTrips.count - 1)
-                        currentTripIndex = newIndex
-                    }
-                    : nil
-            )
+                }
+                .frame(width: outer.size.width, height: outer.size.height, alignment: .leading)
+                .offset(x: -CGFloat(currentTripIndex) * outer.size.width)
+                .offset(x: dragOffset)
+                .gesture(
+                    !isCardTapped ? DragGesture()
+                        .updating($dragOffset) { value, state, transaction in
+                            state = value.translation.width
+                        }
+                        .onEnded { value in
+                            let threshold = outer.size.width * 0.65
+                            var newIndex = Int(-value.translation.width / threshold) + currentTripIndex
+                            newIndex = min(max(newIndex, 0), sampleTrips.count - 1)
+                            currentTripIndex = newIndex
+                        }
+                        : nil
+                )
+            }
+            .animation(.interpolatingSpring(mass: 0.6, stiffness: 100, damping: 10, initialVelocity: 0.3))
         }
-        .animation(.interpolatingSpring(mass: 0.6, stiffness: 100, damping: 10, initialVelocity: 0.3))
     }
 }
 
