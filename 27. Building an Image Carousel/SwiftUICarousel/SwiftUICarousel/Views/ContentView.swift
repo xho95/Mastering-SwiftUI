b0dbdcc217882de4aae8066b2ff9c4b908a54b37
Kim, Min Ho <xho1995@gmail.com>
2021-05-09 22:48:54 +0900

    adds the offset code of the HStack with GeometryReader in the project

--- 27. Building an Image Carousel/SwiftUICarousel/SwiftUICarousel/Views/ContentView.swift
+++ 27. Building an Image Carousel/SwiftUICarousel/SwiftUICarousel/Views/ContentView.swift
@@ -9,23 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isCardTapped = false
+    @State private var currentTripIndex = 2
     
     var body: some View {
         GeometryReader { outer in
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(alignment: .center) {
-                    ForEach(sampleTrips.indices) { index in
-                        GeometryReader { inner in
-                            TripCardView(destination: sampleTrips[index].destination,
-                                         imageName: sampleTrips[index].image,
-                                         isShowDetails: $isCardTapped)
-                        }
-                        .padding(.horizontal, 20)
-                        .frame(width: outer.size.width, height: 450)
+            HStack(spacing: 0) {
+                ForEach(sampleTrips.indices) { index in
+                    GeometryReader { inner in
+                        TripCardView(destination: sampleTrips[index].destination,
+                                     imageName: sampleTrips[index].image,
+                                 isShowDetails: $isCardTapped)
                     }
+                    .padding(.horizontal, self.isCardTapped ? CGFloat(0) : 20)
+                    .frame(width: outer.size.width, height: 500)
                 }
             }
             .frame(width: outer.size.width, height: outer.size.height, alignment: .leading)
+            .offset(x: -CGFloat(currentTripIndex) * outer.size.width)
         }
     }
 }
