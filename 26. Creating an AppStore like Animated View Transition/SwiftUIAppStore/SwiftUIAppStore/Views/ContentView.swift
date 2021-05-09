b66af1e5e633bb0951ca5e2569439c28423024c3
Kim, Min Ho <xho1995@gmail.com>
2021-05-09 16:49:35 +0900

    adds the code of Expanding Card to Full Screen in the project

--- 26. Creating an AppStore like Animated View Transition/SwiftUIAppStore/SwiftUIAppStore/Views/ContentView.swift
+++ 26. Creating an AppStore like Animated View Transition/SwiftUIAppStore/SwiftUIAppStore/Views/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showContents: [Bool] = Array(repeating: false, count: sampleArticles.count)
+    
+    private var contentMode: ContentMode {
+        self.showContents.contains(true) ? .content : .list
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { fullView in
+            ScrollView {
+                VStack(spacing: 40) {
+                    TopBar()
+                        .padding(.horizontal, 20)
+                        .opacity(contentMode == .content ? 0 : 1)
+                    
+                    ForEach(sampleArticles.indices) { index in
+                        GeometryReader { inner in
+                            ArticleCardView(category: sampleArticles[index].category,
+                                            headline: sampleArticles[index].headline,
+                                            subHeadline: sampleArticles[index].subHeadline,
+                                            image: sampleArticles[index].image,
+                                            content: sampleArticles[index].content,
+                                            isShowContent: $showContents[index])
+                                .padding(.horizontal, showContents[index] ? 0 : 20)
+                                .opacity(
+                                    contentMode == .list || (contentMode == .content && showContents[index]) ? 1 : 0
+                                )
+                                .onTapGesture {
+                                    showContents[index] = true
+                                }
+                        }
+                        .frame(height: showContents[index] ?
+                                fullView.size.height + fullView.safeAreaInsets.top + fullView.safeAreaInsets.bottom :
+                                min(sampleArticles[index].image.size.height / 3, 500))
+                    }
+                }
+            }
+        }
+    }
+    
+    enum ContentMode {
+        case list
+        case content
     }
 }
 
---
+++ 26. Creating an AppStore like Animated View Transition/SwiftUIAppStore/SwiftUIAppStore/Views/TopBar.swift
@@ -0,0 +1,55 @@
+//
+//  TopBar.swift
+//  SwiftUIAppStore
+//
+//  Created by Kim, Min Ho on 2021/05/09.
+//
+
+import SwiftUI
+
+struct TopBar: View {
+    var body: some View {
+        HStack(alignment: .lastTextBaseline) {
+            VStack(alignment: .leading) {
+                Text(getCurrentDate().uppercased())
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                Text("Today")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+            }
+            
+            Spacer()
+            
+            Avatar(image: "profile-me", width: 40, height: 40)
+        }
+    }
+    
+    private func getCurrentDate(with format: String = "EEEE, MMM d") -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        return dateFormatter.string(from: Date())
+    }
+}
+
+struct TopBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TopBar()
+    }
+}
+
+struct Avatar: View {
+    let image: String
+    let width: CGFloat
+    let height: CGFloat
+    
+    var body: some View {
+        Image(image)
+            .resizable()
+            .frame(width: width, height: height)
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.gray, lineWidth: 1)
+            )
+    }
+}
