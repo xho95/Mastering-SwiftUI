fd7ef2fcaa51bee41080e7f04fa8b1bcf51b8f85
Kim, Min Ho <xho1995@gmail.com>
2021-05-12 15:59:30 +0900

    initializes the project in the chapter 29 and adds the code using GridItem

---
+++ 29. Building Grid Layouts Using LazyVGrid and LazyHGrid/SwiftUIGrid/SwiftUIGrid/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  SwiftUIGrid
+//
+//  Created by Kim, Min Ho on 2021/05/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    private var symbols = [
+        "keyboard", "hifispeaker.fill", "printer.fill", "tv.fill", "desktopcomputer", "headphones", "tv.music.note", "mic", "plus.bubble", "video"
+    ]
+    
+    private var colors: [Color] = [.yellow, .purple, .green]
+
+//    private var gridItemLayout = [
+//        GridItem(.flexible(), spacing: 0), GridItem(.flexible(), spacing: 0), GridItem(.flexible(), spacing: 0)
+//    ]
+
+    private var gridItemLayout = Array(repeating: GridItem(.flexible(), spacing: 0), count: 3)
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: gridItemLayout, spacing: 0) {
+                ForEach(0...9999, id: \.self) {
+                    Image(systemName: symbols[$0 % symbols.count])
+                        .font(.system(size: 30))
+                        //.frame(width: 50, height: 50)
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 50)
+                        .background(colors[$0 % colors.count])
+                        .cornerRadius(10)
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 29. Building Grid Layouts Using LazyVGrid and LazyHGrid/SwiftUIGrid/SwiftUIGrid/SwiftUIGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGridApp.swift
+//  SwiftUIGrid
+//
+//  Created by Kim, Min Ho on 2021/05/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
