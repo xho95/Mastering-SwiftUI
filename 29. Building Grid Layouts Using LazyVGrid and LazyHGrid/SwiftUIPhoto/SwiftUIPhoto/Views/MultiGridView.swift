6890a01a58740cb007cdd6f8909ea95b6452a06b
Kim, Min Ho <xho1995@gmail.com>
2021-05-12 19:19:10 +0900

    finishes the chapter 29 with two exercises

--- 29. Building Grid Layouts Using LazyVGrid and LazyHGrid/SwiftUIPhoto/SwiftUIPhoto/Views/MultiGridView.swift
+++ 29. Building Grid Layouts Using LazyVGrid and LazyHGrid/SwiftUIPhoto/SwiftUIPhoto/Views/MultiGridView.swift
@@ -11,10 +11,10 @@ struct MultiGridView: View {
     @Environment(\.horizontalSizeClass) var horizontalSizeClass: UserInterfaceSizeClass?
     @Environment(\.verticalSizeClass) var verticalSizeClass: UserInterfaceSizeClass?
 
-    @State private var gridLayout = [
-        GridItem(.adaptive(minimum: 100)),
-        GridItem(.flexible())
-    ]
+    @State private var isCoffeeShown = false
+    @State private var isTwoColumns = false
+
+    @State private var gridLayout = [ GridItem() ]
     
     var body: some View {
         NavigationView {
@@ -29,31 +29,79 @@ struct MultiGridView: View {
                             .cornerRadius(10)
                             .shadow(color: Color.primary.opacity(0.3), radius: 1)
                         
-                        LazyVGrid(columns: [GridItem(.adaptive(minimum: 50))]) {
-                            ForEach(cafe.coffeePhotos) { photo in
-                                Image(photo.name)
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(minWidth: 0, maxWidth: .infinity)
-                                    .frame(height: 50)
-                                    .cornerRadius(10)
+                        if isCoffeeShown {
+                            LazyVGrid(columns: [GridItem(.adaptive(minimum: 50))]) {
+                                ForEach(cafe.coffeePhotos) { photo in
+                                    Image(photo.name)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(minWidth: 0, maxWidth: .infinity)
+                                        .frame(height: 50)
+                                        .cornerRadius(10)
+                                }
                             }
+                            .frame(minHeight: 0, maxHeight: .infinity, alignment: .top)
+                            .animation(.easeIn)
                         }
-                        .frame(minHeight: 0, maxHeight: .infinity, alignment: .top)
-                        .animation(.easeIn)
                     }
                 }
                 .padding(.all, 10)
                 .animation(.interactiveSpring())
             }
             .navigationTitle("Coffe Feed")
-            .onChange(of: verticalSizeClass) { value in
-                self.gridLayout = [
-                    GridItem(.adaptive(minimum: value == .compact ? 250 : 100)),
-                    GridItem(.flexible())
-                ]
+            .navigationBarItems(
+                trailing: HStack {
+                    Button {
+                        isCoffeeShown.toggle()
+                    } label: {
+                        Image(systemName: "squares.below.rectangle")
+                            .font(.title)
+                            .foregroundColor(.primary)
+                    }
+                    
+                    Button {
+                        if gridLayout.count == 1 {
+                            let minWidth: CGFloat = {
+                                switch (horizontalSizeClass, verticalSizeClass) {
+                                case (_, .compact): // iPhone Landscape
+                                    return 250
+                                case (.regular, .regular): // iPad
+                                    return 500
+                                default:
+                                    return 100
+                                }
+                            }()
+
+                            gridLayout = [ GridItem(.adaptive(minimum: minWidth)), GridItem(.flexible()) ]
+                        } else {
+                            gridLayout = [ GridItem() ]
+                        }
+                    } label: {
+                        Image(systemName: "rectangle.grid.3x2")
+                            .font(.title)
+                            .foregroundColor(.primary)
+                    }
+                }
+            )
+        }
+        .onChange(of: verticalSizeClass) { value in
+            gridLayout = [
+                GridItem(.adaptive(minimum: value == .compact ? 250 : 100)),
+                GridItem(.flexible())
+            ]
+        }
+        .onAppear {
+            switch (horizontalSizeClass, verticalSizeClass) {
+            case (.compact, .regular):  // iPhone Portrait
+                gridLayout =  [ GridItem() ]
+            case (_, .compact): // iPhone Landscape
+                gridLayout = [ GridItem(.adaptive(minimum: 250.0)), GridItem(.flexible()) ]
+            case (.regular, .regular): // iPad
+                gridLayout = [ GridItem(.adaptive(minimum: 500.0)), GridItem(.flexible()) ]
+            default: break
             }
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
