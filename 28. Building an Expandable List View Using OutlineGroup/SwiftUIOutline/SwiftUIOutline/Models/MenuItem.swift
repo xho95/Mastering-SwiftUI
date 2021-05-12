eeb259693f134ccdc2b02267fa0b3e6769a3fdda
Kim, Min Ho <xho1995@gmail.com>
2021-05-12 14:08:17 +0900

    initializes the project in the chapter 28 and sets up the Model with downloaded Images

---
+++ 28. Building an Expandable List View Using OutlineGroup/SwiftUIOutline/SwiftUIOutline/Models/MenuItem.swift
@@ -0,0 +1,63 @@
+//
+//  MenuItem.swift
+//  SwiftUIOutline
+//
+//  Created by Kim, Min Ho on 2021/05/12.
+//
+
+import Foundation
+
+struct MenuItem: Identifiable {
+    var id = UUID()
+    var name: String
+    var image: String
+    var subMenuItems: [MenuItem]?
+}
+
+// Main menu items
+let sampleMenuItems = [
+    MenuItem(name: "Espresso Machines", image: "linea-mini", subMenuItems: espressoMachineMenuItems),
+    MenuItem(name: "Grinders", image: "swift-mini", subMenuItems: grinderMenuItems),
+    MenuItem(name: "Other Equipment", image: "espresso-ep", subMenuItems: otherMenuItems),
+]
+
+// Sub-menu items for Espresso Machines
+let espressoMachineMenuItems = [
+    MenuItem(name: "Leva", image: "leva-x", subMenuItems: [
+        MenuItem(name: "Leva X", image: "leva-x"),
+        MenuItem(name: "Leva S", image: "leva-s"),
+    ]),
+    MenuItem(name: "Strada", image: "strada-ep", subMenuItems: [
+        MenuItem(name: "Strada EP", image: "strada-ep"),
+        MenuItem(name: "Strada AV", image: "strada-av"),
+        MenuItem(name: "Strada MP", image: "strada-mp"),
+        MenuItem(name: "Strada EE", image: "strada-ee"),
+    ]),
+    MenuItem(name: "KB90", image: "kb90"),
+    MenuItem(name: "Linea", image: "linea-pb-x", subMenuItems: [
+        MenuItem(name: "Linea PB X", image: "linea-pb-x"),
+        MenuItem(name: "Linea PB", image: "linea-pb"),
+        MenuItem(name: "Linea Classic", image: "linea-classic"),
+    ]),
+    MenuItem(name: "GB5", image: "gb5"),
+    MenuItem(name: "Home", image: "gb3", subMenuItems: [
+        MenuItem(name: "GB3", image: "gb3"),
+        MenuItem(name: "Linea Mini", image: "linea-mini"),
+    ]),
+]
+
+// Sub-menu items for Grinder
+let grinderMenuItems = [
+    MenuItem(name: "Swift", image: "swift"),
+    MenuItem(name: "Vulcano", image: "vulcano"),
+    MenuItem(name: "Swift Mini", image: "swift-mini"),
+    MenuItem(name: "Lux D", image: "lux-d"),
+]
+
+// Sub-menu items for other equipment
+let otherMenuItems = [
+    MenuItem(name: "Espresso AV", image: "espresso-av"),
+    MenuItem(name: "Espresso EP", image: "espresso-ep"),
+    MenuItem(name: "Pour Over", image: "pourover"),
+    MenuItem(name: "Steam", image: "steam"),
+]
---
+++ 28. Building an Expandable List View Using OutlineGroup/SwiftUIOutline/SwiftUIOutline/SwiftUIOutlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIOutlineApp.swift
+//  SwiftUIOutline
+//
+//  Created by Kim, Min Ho on 2021/05/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIOutlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
