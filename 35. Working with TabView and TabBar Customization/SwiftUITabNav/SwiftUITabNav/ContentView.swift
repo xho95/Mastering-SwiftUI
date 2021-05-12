dc71b919909b1d10903646fc09eaac52cdf20a5a
Kim, Min Ho <xho1995@gmail.com>
2021-05-13 00:01:15 +0900

    adds the Tab Bar showing code out of the NavigationView

---
+++ 35. Working with TabView and TabBar Customization/SwiftUITabNav/SwiftUITabNav/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  SwiftUITabNav
+//
+//  Created by Kim, Min Ho on 2021/05/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+    
+    init() {
+        UITabBar.appearance().barTintColor = .white
+    }
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            NavigationView {
+                List(1...10, id: \.self) { index in
+                    NavigationLink(
+                        destination: Text("Item #\(index) Details"),
+                        label: {
+                            Text("Item #\(index)")
+                                .font(.system(size: 20, weight: .bold, design: .rounded))
+                        }
+                    )
+                }
+                .navigationTitle("TabView Demo")
+            }
+            .tabItem {
+                Image(systemName: "house.fill")
+                Text("Home")
+            }
+            .tag(0)
+            
+            Text("Bookmark Tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "bookmark.circle.fill")
+                    Text("Bookmark")
+                }
+                .tag(1)
+            Text("Video Tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "video.circle.fill")
+                    Text("Video")
+                }
+                .tag(2)
+            Text("Profile Tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "person.crop.circle")
+                    Text("Profile")
+                }
+                .tag(3)
+        }
+        .accentColor(.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
