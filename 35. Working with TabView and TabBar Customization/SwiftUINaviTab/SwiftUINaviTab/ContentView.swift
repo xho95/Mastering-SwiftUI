78fb2e87af765199e3278790d7c8e27447c470b1
Kim, Min Ho <xho1995@gmail.com>
2021-05-12 23:50:32 +0900

    initializes the TabBar project with a NavigationView

---
+++ 35. Working with TabView and TabBar Customization/SwiftUINaviTab/SwiftUINaviTab/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  SwiftUINaviTab
+//
+//  Created by Kim, Min Ho on 2021/05/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+    
+    init() {
+        UITabBar.appearance().barTintColor = .white
+    }
+    
+    var body: some View {
+        NavigationView {
+            TabView(selection: $selection) {
+                Text("Home Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Home")
+                    }
+                    .tag(0)
+                Text("Bookmark Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "bookmark.circle.fill")
+                        Text("Bookmark")
+                    }
+                    .tag(1)
+                Text("Video Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "video.circle.fill")
+                        Text("Video")
+                    }
+                    .tag(2)
+                Text("Profile Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "person.crop.circle")
+                        Text("Profile")
+                    }
+                    .tag(3)
+            }
+            .accentColor(.red)
+            .navigationTitle("TabView Demo")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 35. Working with TabView and TabBar Customization/SwiftUINaviTab/SwiftUINaviTab/SwiftUINaviTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINaviTabApp.swift
+//  SwiftUINaviTab
+//
+//  Created by Kim, Min Ho on 2021/05/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINaviTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
