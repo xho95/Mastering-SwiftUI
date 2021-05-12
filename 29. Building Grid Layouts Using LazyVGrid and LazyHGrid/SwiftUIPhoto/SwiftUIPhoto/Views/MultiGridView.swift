398aae448dafcc95786787eaf4b48319452ca5e3
Kim, Min Ho <xho1995@gmail.com>
2021-05-12 18:25:25 +0900

    adds an Additional LazyVGrid in the original one of the project

--- 29. Building Grid Layouts Using LazyVGrid and LazyHGrid/SwiftUIPhoto/SwiftUIPhoto/Views/MultiGridView.swift
+++ 29. Building Grid Layouts Using LazyVGrid and LazyHGrid/SwiftUIPhoto/SwiftUIPhoto/Views/MultiGridView.swift
@@ -22,6 +22,19 @@ struct MultiGridView: View {
                             .frame(height: 150)
                             .cornerRadius(10)
                             .shadow(color: Color.primary.opacity(0.3), radius: 1)
+                        
+                        LazyVGrid(columns: [GridItem(.adaptive(minimum: 50))]) {
+                            ForEach(cafe.coffeePhotos) { photo in
+                                Image(photo.name)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(minWidth: 0, maxWidth: .infinity)
+                                    .frame(height: 50)
+                                    .cornerRadius(10)
+                            }
+                        }
+                        .frame(minHeight: 0, maxHeight: .infinity, alignment: .top)
+                        .animation(.easeIn)
                     }
                 }
                 .padding(.all, 10)
