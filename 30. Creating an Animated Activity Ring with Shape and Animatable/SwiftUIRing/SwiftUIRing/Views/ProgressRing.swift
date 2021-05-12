caeb06ec6fff7a9129be831297abe72ca7e7387d
Kim, Min Ho <xho1995@gmail.com>
2021-05-12 19:56:50 +0900

    adds an AngularGradient code in the project

--- 30. Creating an Animated Activity Ring with Shape and Animatable/SwiftUIRing/SwiftUIRing/Views/ProgressRing.swift
+++ 30. Creating an Animated Activity Ring with Shape and Animatable/SwiftUIRing/SwiftUIRing/Views/ProgressRing.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ProgressRing: View {
     var thickness: CGFloat = 30.0
     var width: CGFloat = 250.0
-    var gradient = Gradient(colors: [.darkRed, .lightRed])
+    var gradient = Gradient(colors: [.dardPurple, .lightYellow])
+    var startAngle = -90.0
     
     var body: some View {
         ZStack {
@@ -18,6 +19,9 @@ struct ProgressRing: View {
                 .stroke(Color(.systemGray6), lineWidth: thickness)
             
             RingShape(progress: 0.5, thickness: thickness)
+                .fill(AngularGradient(gradient: gradient, center: .center,
+                                      startAngle: .degrees(startAngle),
+                                      endAngle: .degrees(startAngle + 360 * 0.5)))
         }
         .frame(width: width, height: width, alignment: .center)
     }
