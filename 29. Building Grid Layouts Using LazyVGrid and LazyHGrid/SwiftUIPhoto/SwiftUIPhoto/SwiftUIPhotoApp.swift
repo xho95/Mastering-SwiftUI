cbd8e5e0f73bbd222350b01725e4ab6704708a68
Kim, Min Ho <xho1995@gmail.com>
2021-05-12 16:17:39 +0900

    initializes the project of the chapter 29 with the PhotoGrid

---
+++ 29. Building Grid Layouts Using LazyVGrid and LazyHGrid/SwiftUIPhoto/SwiftUIPhoto/SwiftUIPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPhotoApp.swift
+//  SwiftUIPhoto
+//
+//  Created by Kim, Min Ho on 2021/05/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
