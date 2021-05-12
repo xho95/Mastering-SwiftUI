db0860722d4b637b856f4a4b14c6aaecd75b4af3
Kim, Min Ho <xho1995@gmail.com>
2021-05-12 19:37:48 +0900

    initializes the project in the chapter 30 with a Color Extension

---
+++ 30. Creating an Animated Activity Ring with Shape and Animatable/SwiftUIRing/SwiftUIRing/SwiftUIRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRingApp.swift
+//  SwiftUIRing
+//
+//  Created by Kim, Min Ho on 2021/05/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 30. Creating an Animated Activity Ring with Shape and Animatable/SwiftUIRing/SwiftUIRing/Views/ProgressRing.swift
@@ -0,0 +1,20 @@
+//
+//  ProgressRing.swift
+//  SwiftUIRing
+//
+//  Created by Kim, Min Ho on 2021/05/12.
+//
+
+import SwiftUI
+
+struct ProgressRing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProgressRing_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressRing()
+    }
+}
