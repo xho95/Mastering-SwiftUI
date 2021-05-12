83d5bc05791e1f1d7963e0513257d67eb9611428
Kim, Min Ho <xho1995@gmail.com>
2021-05-12 23:44:01 +0900

    adds the code switching between Tabs programmatically

--- 35. Working with TabView and TabBar Customization/SwiftUITabView/SwiftUITabView/ContentView.swift
+++ 35. Working with TabView and TabBar Customization/SwiftUITabView/SwiftUITabView/ContentView.swift
@@ -8,38 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = 0
+    
     init() {
-        UITabBar.appearance().barTintColor = .yellow
+        UITabBar.appearance().barTintColor = .white
     }
     
     var body: some View {
-        TabView {
-            Text("Home Tab")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "house.fill")
-                    Text("Home")
-                }
-            Text("Bookmark Tab")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "bookmark.circle.fill")
-                    Text("Bookmark")
-                }
-            Text("Video Tab")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "video.circle.fill")
-                    Text("Video")
-                }
-            Text("Profile Tab")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "person.crop.circle")
-                    Text("Profile")
-                }
+        ZStack(alignment: .topTrailing) {
+            TabView(selection: $selection) {
+                Text("Home Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Home")
+                    }
+                    .tag(0)
+                Text("Bookmark Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "bookmark.circle.fill")
+                        Text("Bookmark")
+                    }
+                    .tag(1)
+                Text("Video Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "video.circle.fill")
+                        Text("Video")
+                    }
+                    .tag(2)
+                Text("Profile Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "person.crop.circle")
+                        Text("Profile")
+                    }
+                    .tag(3)
+            }
+            .accentColor(.red)
+
+            Button {
+                selection = (selection + 1) % 4
+            } label: {
+                Text("Next")
+                    .font(.system(.headline, design: .rounded))
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.red)
+                    .cornerRadius(10.0)
+                    .padding()
+            }
         }
-        .accentColor(.red)
     }
 }
 
