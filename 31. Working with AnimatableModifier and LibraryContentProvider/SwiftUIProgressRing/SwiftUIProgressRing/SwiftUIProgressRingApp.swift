8986e1c2739eb933e5b90df1e23d25d47ec3601c
Kim, Min Ho <xho1995@gmail.com>
2021-05-12 22:10:26 +0900

    downloads and initializes the project in the chapter 31

---
+++ 31. Working with AnimatableModifier and LibraryContentProvider/SwiftUIProgressRing/SwiftUIProgressRing/SwiftUIProgressRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProgressRingApp.swift
+//  SwiftUIProgressRing
+//
+//  Created by Simon Ng on 27/7/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProgressRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
