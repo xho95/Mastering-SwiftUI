d6e7ad38691bbe621bd8bb0a6499601b18f315ae
Kim, Min Ho <xho1995@gmail.com>
2021-05-12 14:34:32 +0900

    customizes the Expandable List using OutlineGroup

--- 28. Building an Expandable List View Using OutlineGroup/SwiftUIOutline/SwiftUIOutline/Views/ContentView.swift
+++ 28. Building an Expandable List View Using OutlineGroup/SwiftUIOutline/SwiftUIOutline/Views/ContentView.swift
@@ -9,16 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List(sampleMenuItems, children: \.subMenuItems) { item in
-            HStack {
-                Image(item.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 50, height: 50)
-                
-                Text(item.name)
-                    .font(.system(.title3, design: .rounded))
-                    .bold()
+        List {
+            ForEach(sampleMenuItems) { menuItem in
+                Section(
+                    header: HStack {
+                        Text(menuItem.name)
+                            .font(.title3)
+                            .fontWeight(.heavy)
+                        
+                        Image(menuItem.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30, height: 30)
+                    }
+                    .padding(.vertical)
+                ) {
+                    OutlineGroup(menuItem.subMenuItems ?? [MenuItem](), children: \.subMenuItems) { item in
+                        HStack {
+                            Image(item.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                            
+                            Text(item.name)
+                                .font(.system(.title3, design: .rounded))
+                                .bold()
+                        }
+                    }
+                }
             }
         }
         .listStyle(InsetGroupedListStyle())
