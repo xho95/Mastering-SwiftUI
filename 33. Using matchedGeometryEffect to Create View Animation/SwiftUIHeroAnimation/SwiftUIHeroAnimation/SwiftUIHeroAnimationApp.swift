d4426febedf7381cc34c0647fb1514ff253126de
Kim, Min Ho <xho1995@gmail.com>
2021-05-13 13:47:20 +0900

    makes the Hero Animation with MatchedGeometryEffect

---
+++ 33. Using matchedGeometryEffect to Create View Animation/SwiftUIHeroAnimation/SwiftUIHeroAnimation/SwiftUIHeroAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHeroAnimationApp.swift
+//  SwiftUIHeroAnimation
+//
+//  Created by Kim, Min Ho on 2021/05/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHeroAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
